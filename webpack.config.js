c96f54220fcc3d788994eee0e82402b5d63da9c7
hai3460377 <281450938@qq.com>
2017-02-17 16:37:37 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var autoprefixer = require('autoprefixer');//npm install autoprefixer --save-dev 自动加css前缀
-var path = require("path");
 module.exports = {
     devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
     entry: __dirname + "/develop/main.js",//已多次提及的唯一入口文件
@@ -21,7 +20,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 loader: ExtractTextPlugin.extract({
-                    fallbackLoader:"style-loader",loader:"css-loader"
+                    fallback:"style-loader",use:"css-loader"
                 })//添加对样式表的处理
             },
             {test: /\.html$/, loader: 'raw'}
@@ -34,6 +33,6 @@ module.exports = {
         new webpack.optimize.UglifyJsPlugin(),//压缩代码插件
         new ExtractTextPlugin({
             filename:"qtopo.css",allChunks: true
-        }),//合并并压缩输出到目录
+        })//合并并压缩输出到目录
     ]
 };
