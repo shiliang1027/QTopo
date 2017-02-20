597a12d00565bc8feec892d9db59e8cd45b4fbba
hai3460377 <281450938@qq.com>
2017-02-20 12:20:31 +0800

    add

---
+++ develop/constant.js
@@ -0,0 +1,16 @@
+/**
+ * Created by qiyc on 2017/2/20.
+ */
+module.exports = {
+    SCENE:'10',
+    CONTAINER:"100",
+    LINK:"1000",
+    NODE:'10000',
+    CASUAL:"临时工",//
+    mode:{
+        NORMAL:"normal",
+        EDIT:"edit",
+        DRAG:"drag",
+        SELECT:"select"
+    }
+};
