338375b30601f58e9ebc032df8ed157784318815
hai3460377 <281450938@qq.com>
2017-04-06 17:34:46 +0800

    文档说明

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 基于jtopo的topo图形绘制工具
 
 show：https://hai3460377.github.io/QTopo/public/index.html
+docs：https://hai3460377.github.io/QTopo/docs/index.html
---
+++ docs/api.js
@@ -0,0 +1,27 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Element",
+        "QTopo",
+        "QTopo.instance",
+        "QTopo.util",
+        "Scene",
+        "option配置"
+    ],
+    "modules": [
+        "QTopo",
+        "core"
+    ],
+    "allModules": [
+        {
+            "displayName": "core",
+            "name": "core"
+        },
+        {
+            "displayName": "QTopo",
+            "name": "QTopo"
+        }
+    ],
+    "elements": []
+} };
+});
